a0b87d5f814acc4967524d6159a6530de398127b
CTTBOT <chanhchung89@gmail.com>
2017-04-06 03:17:26 +0700

    Lib

---
+++ Lib/VNSHARP 2.0/Menu/KeybindSetStage.cs
@@ -0,0 +1,11 @@
+﻿namespace VNSHARP
+{
+    enum KeybindSetStage
+    {
+        Keybind1,
+
+        Keybind2,
+
+        NotSetting
+    }
+}
---
+++ Lib/VNSHARP 2.0/Utility.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using EloBuddy;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VNSHARP
+{
+    public static class Utility
+    {
+        /// <summary>
+        ///     Returns if the GameObject is valid
+        /// </summary>
+        public static bool IsValid<T>(this GameObject obj) where T : GameObject
+        {
+            return obj is T && obj.IsValid;
+        }
+
+        public static class DelayAction
+        {
+            #region Static Fields
+
+            public static List<Action> ActionList = new List<Action>();
+
+            #endregion
+
+            #region Constructors and Destructors
+
+            static DelayAction()
+            {
+                Game.OnTick += GameOnOnGameUpdate;
+            }
+
+            #endregion
+
+            #region Delegates
+
+            public delegate void Callback();
+
+            #endregion
+
+            #region Public Methods and Operators
+
+            public static void Add(int time, Callback func)
+            {
+                var action = new Action(time, func);
+                ActionList.Add(action);
+            }
+
+            #endregion
+
+            #region Methods
+
+            private static void GameOnOnGameUpdate(EventArgs args)
+            {
+                for (var i = ActionList.Count - 1; i >= 0; i--)
+                {
+                    if (ActionList[i].Time <= Utils.GameTimeTickCount)
+                    {
+                        try
+                        {
+                            if (ActionList[i].CallbackObject != null)
+                            {
+                                ActionList[i].CallbackObject();
+                                //Will somehow result in calling ALL non-internal marked classes of the called assembly and causes NullReferenceExceptions.
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            // ignored
+                        }
+
+                        ActionList.RemoveAt(i);
+                    }
+                }
+            }
+
+            #endregion
+
+            public struct Action
+            {
+                #region Fields
+
+                public Callback CallbackObject;
+
+                public int Time;
+
+                #endregion
+
+                #region Constructors and Destructors
+
+                public Action(int time, Callback callback)
+                {
+                    this.Time = time + Utils.GameTimeTickCount;
+                    this.CallbackObject = callback;
+                }
+
+                #endregion
+            }
+        }
+    }
+}
